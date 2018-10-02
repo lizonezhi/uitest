0ca135af78b7f6e3c7d2918b077f6e8ca7cf1253
lizonezhi <lzz136313283@gmail.com>
2018-10-02 22:31:29 +0800

    版本：0.0.9

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
     name='uitest',
-    version='0.0.8',
+    version='0.0.9',
     packages=['uitest'],
-    url='https://github.com/lizongzi/uitest.git',
+    url='https://github.com/lizonezhi/uitest.git',
     license='1.0',
-    author='lizongzi',
+    author='李宗旨',
     author_email='136313283@qq.com',
     description='基于adb的安卓自动化,功能精简用于打包成exe'
 )
